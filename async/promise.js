f46e70e21e410fe0c918cc59aab5d86e30b4ee58
cuijie <yinsu@foxmail.com>
2021-04-17 16:20:10 +0800

    修改代码结构

---
+++ async/promise.js
@@ -0,0 +1,10 @@
+//柏视医疗面试题目
+setImmediate(() => console.log(2));
+setTimeout(() => console.log(1));
+setTimeout(() => console.log(11));
+process.nextTick(() => console.log(31));
+setImmediate(() => console.log(21));
+process.nextTick(() => console.log(3));
+Promise.resolve().then(() => console.log(4));
+Promise.resolve().then(() => console.log(41));
+(() => console.log(5))();
